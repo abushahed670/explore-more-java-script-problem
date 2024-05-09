443f89268de027031e90f381f3e21a76ab037d40
abushahed670 <abu.shahed670@gmail.com>
2024-05-10 00:08:49 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,11 @@
+const country = 'bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = {id: 1231, class: 11, name: 'agun' };
+const friends =[13, 14, 24, 15, 17];
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof friends);
