ed75d6f6981e828af25f6bc9ca4693442931d4fc
abushahed670 <abu.shahed670@gmail.com>
2024-05-10 12:31:01 +0600

    add elements in splice

--- splice.js
+++ splice.js
@@ -1,4 +1,5 @@
-const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
-const partial = friends.splice(2, 5);
+const friends = [12, 45, 32, 22, 48, 62, 29, 69, 87];
+// const partial = friends.splice(2, 5);
+const partial = friends.splice(2, 5, 44, 55, 66);
 console.log(partial);
 console.log(friends);
