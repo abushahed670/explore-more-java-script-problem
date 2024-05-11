05d62d9a777f36665acdc1d8b468434129de09b6
abushahed670 <abu.shahed670@gmail.com>
2024-05-11 21:58:52 +0600

    quality and quantity calculation

---
+++ wood.js
@@ -0,0 +1,14 @@
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+
+    const chairWood = chairQuantity * perChairWood;
+    const tableWood = tableQuantity * perTableWood;
+    const bedWood = bedQuantity * perBedWood;
+    // console.log(perChairWood, perTableWood, perBedWood);
+     const totalWood = chairWood + tableWood + bedWood;
+     return totalWood;
+}
+const totalWood = woodCalculator(2, 2, 5);
+console.log('total wood required:', totalWood);
